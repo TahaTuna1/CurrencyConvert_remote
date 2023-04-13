8296383127b5ef6c3574d280bd6806ad0eb42cb2
Taha Tuna <tahatuna777@gmail.com>
2023-04-13 20:42:16 +0200

    Initial Commit

---
+++ CurrencyConvert/CurrencyConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConvertApp.swift
+//  CurrencyConvert
+//
+//  Created by Taha Tuna on 13.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
