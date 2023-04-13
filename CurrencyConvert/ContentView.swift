20a6434a48af070cde7ee11cea7f10a71493b1b9
Taha Tuna <tahatuna777@gmail.com>
2023-04-14 00:31:26 +0200

    Basic Layout Update

--- CurrencyConvert/ContentView.swift
+++ CurrencyConvert/ContentView.swift
@@ -10,70 +10,129 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack(alignment: .top) {
-            Color("primaryCustom").ignoresSafeArea()
+            Color(.black).opacity(0.9).ignoresSafeArea()
             
             
             VStack {
                 VStack {
                     HStack{//Top Icons
                         Image(systemName: "person")
-                            .padding(20)
                         Spacer()
                         Image(systemName: "directcurrent")
-                            .padding(20)
                         Spacer()
                         Image(systemName: "gear")
-                            .padding(20)
-                    }
+                    }.padding(10)
                     .font(.largeTitle)
-                    Spacer()
                     
-                    HStack{// Main Currency
-                        Text("$")
-                        Spacer()
-                        VStack{
-                            Text("8,650.11")
+                    VStack{// Main Currency
+                        HStack{
+                            Text("$")
+                            Spacer()
+                            Text("8,650")
                                 .font(.custom("mainCurrency", size: 45))
-                            Text("USD")
-                                .font(.title3)
+                            Spacer()
+                            Image(systemName: "arrowtriangle.down")
+                                .font(.title2)
                         }
-                        Spacer()
-                        Image(systemName: "arrowtriangle.down")
-                            .font(.title2)
-                        
+                        Text("USD")
+                            .font(.title3).foregroundColor(.gray)
                     }
                     .font(.largeTitle)
                     .padding(30)
                     .fontWeight(.light)
                 }
-                .frame(minHeight: 200, maxHeight: 250)
+                .frame(minHeight: 80, maxHeight: 200)
                 .background(Color.accentColor.ignoresSafeArea())
+                .padding(.bottom, 30)
                 
                 
-               Spacer()
+                
+                SecondaryCurrencyView(currencyIcon: "€", currencyName: "Euro", amount: 7824)
+                SecondaryCurrencyView(currencyIcon: "₺", currencyName: "Lira", amount: 167291)
+                SecondaryCurrencyView(currencyIcon: "₽", currencyName: "Ruble", amount: 707463)
                 
                 
-                HStack{
-                    HStack{
-                        VStack{
-                            Text("€")
-                            Text("Euro").font(.body)
-                        }
-                        Image(systemName: "arrowtriangle.down").font(.body)
-                    }
-                    
+//                Button {
+//                    //Nothing yet
+//                } label: {
+//                    Text("Add New Currency")
+//                        .font(.body)
+//                        .foregroundColor(.white)
+//                        .frame(width: 190, height: 70)
+//                        .background(Color.blue.opacity(0.1))
+//                        .cornerRadius(radius: 40, corners: [.bottomLeft, .topRight])
+//                }
+                
+                VStack {
+                    Text("$ USD - Euro €").foregroundColor(.gray).padding(.top, 10)
                     Spacer()
-                    
-                    Text("€7,824.79")
+                    Image("graphPlaceHolder")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .padding(.bottom, 40)
+                        
                 }
-                .foregroundColor(.white)
-                .padding(35)
-                .font(.largeTitle)
-                .fontWeight(.light)
+                .frame(minWidth: 300, maxWidth: 370, minHeight: 100, maxHeight: 170)
+                .background(
+                    LinearGradient(
+                        gradient: Gradient(stops: [
+                            .init(color: Color.white.opacity(0.05), location: 0),
+                            .init(color: Color.white.opacity(0), location: 1)
+                        ]),
+                        startPoint: .leading,
+                        endPoint: .trailing
+                    )
+                )
+                .cornerRadius(30)
+                
             }
         }
     }
 }
+
+
+
+
+struct SecondaryCurrencyView: View{
+    var currencyIcon: String
+    var currencyName: String
+    var amount: Int
+    var body: some View{
+        HStack{
+            HStack{
+                VStack{
+                    Text(currencyIcon)
+                    Text(currencyName).font(.body).foregroundColor(.gray)
+                }
+                Image(systemName: "arrowtriangle.down").font(.callout).padding(10)
+            }
+            
+            Spacer()
+
+            
+            Text(currencyIcon + NumberFormatter.localizedString(from: NSNumber(value: amount), number: .decimal)).font(.title)
+                
+        }
+        .foregroundColor(.white)
+        .padding(35)
+        .font(.largeTitle)
+        .fontWeight(.light)
+        .frame(width: 370, height: 110)
+        .background(
+            LinearGradient(
+                gradient: Gradient(stops: [
+                    .init(color: Color.white.opacity(0.05), location: 0),
+                    .init(color: Color.white.opacity(0), location: 1)
+                ]),
+                startPoint: .leading,
+                endPoint: .trailing
+            )
+        )
+        .cornerRadius(30)
+    }
+}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
